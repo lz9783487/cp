b697141b92ff8d86b1babc21dddb93a5972d1c9f
asdf1232 <lz9783487@gmail.com>
2025-11-01 16:25:03 +0800

    Refactor Dijkstra's algorithm in dij.cpp
    Refactor Dijkstra's algorithm implementation for clarity and efficiency.

--- graph/dij.cpp
+++ graph/dij.cpp
@@ -1,25 +1,17 @@
-using T = pair<int, int>;
-int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
+vector<int> dis, vis;
+priority_queue<T, vector<T>, greater<T>> pq;
 
-    vector<vector<int>> g;
-    vector<int> dis, vis;
-    priority_queue<T, vector<T>, greater<T>> pq;
+dis[start] = ...;
+pq.emplace(dis[start], start);
 
-    dis[0] = ...;
-    pq.emplace(0, dis[0]);
-    
-    while (!pq.empty()) {
-        auto [d, u] = pq.top(); pq.pop();
-        if (vis[u]) continue;
-        vis[u] = 1; // 如果不适用vis，只是用dis判断，可能由多个dis[u]相等的u点进入堆，导致u点重复扩展多次
-        for (auto i : g[u]) {
-            if (dis[i] > dis[u] + D(u, i)) {
-                dis[i] = dis[u] + D(u, i);
-                pq.emplace(i, dis[i]);
-            }
+while (!pq.empty()) {
+    auto [d, u] = pq.top(); pq.pop();
+    if (vis[u]) continue;
+    vis[u] = 1; // 如果不适用vis，只是用dis判断，可能由多个dis[u]相等的u点进入堆，导致u点重复扩展多次
+    for (auto i : g[u]) {
+        if (dis[v] > dis[u] + D(u, v)) {
+            dis[v] = dis[u] + D(u, v);
+            pq.emplace(dis[v], v);
         }
     }
-    return 0;
 }
